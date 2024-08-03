6a2eec231d8ec5997fe69db3138dc56647dc7c76
lelitoa <adam.lelito@microsoft.wsei.edu.pl>
2024-08-03 17:54:18 +0200

    start build + posteman

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+
+
+async function registerUser(req,res){
+    let data = req.body;
+    console.log(data);
+    res.send("success");
+}
+
+const AuthController = {
+    registerUser
+}
+
+module.exports = AuthController;
