23054093de94d4199bdc5ef7d569e41a6ed583e6
lelitoa <adam.lelito@microsoft.wsei.edu.pl>
2024-08-06 12:29:19 +0200

    logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { getUserDetails } from '../util/GetUser';
+import { Dropdown } from 'antd';
+import avatar from '../assets/login.png';
 
 function Navbar({active}) {
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+  
+  useEffect(()=>{
+    const userDetails = getUserDetails();
+    setUser(userDetails);
+  },[]);
+
+  const handleLogut = ()=>{
+    localStorage.removeItem('toDoAppUser');
+    navigate('/login');
+  }
+
+  const items = [
+   {
+    key: '1',
+    label: (
+      <span onClick={ handleLogut }> Logout </span>
+    ),
+   }
+  ];
+  
   return (
     <header>
       <nav>
@@ -13,14 +38,31 @@ function Navbar({active}) {
         <ul className="navigation-menu">
               
         <li><Link to="/" className={active==='home' && 'activeNav'}>Home</Link></li>
+        {user && <li><Link to="/to-do-list" className={active==='myTask' && 'activeNav'}>My Task</Link></li>}
 
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>      
+        {user ?
+         
+          <Dropdown
+            menu={{items, }}
+            placement="bottom"
+            arrow
+          >
+            <div className='userInfoNav'>
+              <img src={ avatar } alt="." />
+              <span>{ user?.firstName ? `Hello, ${ user?.firstName } ${ user?.lastName }` : user?.username }</span>
+            </div>
+          </Dropdown>
+        : 
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+          </>
+        }
           
         </ul>
       </nav>
     </header>
-)
+  )
 }
 
 export default Navbar
--- client/src/pages/ToDo/ToDoList.jsx
+++ client/src/pages/ToDo/ToDoList.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 function ToDoList() {
   return (
-  
-  <div>ToDoList</div>
+  <>
+    <Navbar active={"myTask"} />
+  </>
 )
 }
 
