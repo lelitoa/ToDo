11e548e2d79ceb6c22d6618c8c0e8a5d27d4bd35
lelitoa <adam.lelito@microsoft.wsei.edu.pl>
2024-08-06 18:59:31 +0200

    toDo api v2

---
+++ client/src/services/toDoServices.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { getUserDetails } from '../util/GetUser';
+
+const SERVER_URL = 'http://localhost:5000/api/todo';
+
+const authHeaders = ()=>{
+    let userToken = getUserDetails()?.token;
+    return {headers: {'Authorization:': userToken}}
+}
+
+const createToDo = (data)=>{
+    return axios.post(SERVER_URL+'/create-to-do', data, authHeaders());
+}
+
+const getAllToDo = (userId)=>{
+    return axios.post(SERVER_URL+'/get-all-to-do/' + userId, authHeaders());
+}
+
+const deleteToDo = (id)=>{
+    return axios.post(SERVER_URL+'/delete-to-do/' + id, authHeaders());
+}
+
+const updateToDo = (id, data)=>{
+    return axios.post(SERVER_URL+'/update-to-do/' + id, data, authHeaders());
+}
+
+const ToDoServices = {
+    createToDo,
+    getAllToDo,
+    deleteToDo,
+    updateToDo
+}
+
+export default ToDoServices;
