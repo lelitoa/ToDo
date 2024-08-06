f467e62ece55e10d22adb7608ef4fa8590eadb25
lelitoa <adam.lelito@microsoft.wsei.edu.pl>
2024-08-06 16:37:37 +0200

    toDo api

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -12,3 +12,40 @@ exports.createToDo = async (req,res)=>{
         res.status(err);
     }
 }
+
+exports.getAllToDo = async (req,res) =>{
+    let {userId} = req.params;
+
+    try{
+        const result = await ToDo.find({createdBy: userId});
+        res.send(result);
+    } catch{err}{
+        console.log(err);
+        res.status(400).send(err);
+    }
+}
+
+exports.updateToDo = async (req,res) =>{
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const result = await ToDo.findByIdAndUpdate(id, {$set: data}, {returnOriginal: false});
+        console.log(result);
+        res.send({message: 'ToDo list Updated!'})
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+    }
+}
+
+exports.deleteToDo = async (req,res) =>{
+    try {
+        const { id } = req.params;
+        const result = await ToDo.findByIdAndDelete(id);
+        console.log(result);
+        res.send({ message: "ToDo Task Deleted!" });
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+    }
+}
